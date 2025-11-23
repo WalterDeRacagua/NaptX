29318f3e0f5561c9f620c392f032fefba07809e1
Sergio Carrasco <sesanc15@ucm.es>
2025-11-23 21:11:12 +0100

    Comenzando la conexión a Sepolia testnet

--- app/src/main/java/com/example/offlinepaymentsystem/data/blockchain/Web3Manager.java
+++ app/src/main/java/com/example/offlinepaymentsystem/data/blockchain/Web3Manager.java
@@ -1,4 +1,11 @@
 package com.example.offlinepaymentsystem.data.blockchain;
 
+import org.web3j.protocol.Web3j;
+
 public class Web3Manager {
+
+    private static final String TAG = "Web3Manager";
+    private static  Web3Manager instance;
+    private Web3j web3j;
+    private boolean isConnected = false;
 }
